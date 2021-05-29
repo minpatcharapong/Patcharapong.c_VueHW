8dd980558b086dfc2c7df25f1e90f4edacff821c
minpatcharapong <84860581+minpatcharapong@users.noreply.github.com>
2021-05-29 19:58:51 +0700

    Initial commit

---
+++ src/pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-8">
+    <figure class="bg-gray-100 rounded-xl p-8 shadow-md">
+      <div class="pt-6 text-center space-y-4">
+        <figcaption class="font-medium">
+          <div class="text-gray-500 text-center">
+            <li>Tel : {{ tel }}</li>
+            <li>E-mail : {{ email }}</li>
+            <li>Facebook : {{ facebook }}</li>
+            <li>Line : {{ idline }}</li>
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: "ch.patcharapong_st@tni.ac.th",
+    facebook: "Min Patcharapong",
+    tel: "097-181-5647",
+    idline: "minpatcharapong",
+  }),
+};
+</script>
---
+++ src/pages/info.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="p-8">
+    <figure class="xl:flex bg-gray-100 rounded-xl p-8 shadow-md">
+      <div class="pt-6 text-center space-y-4">
+        <div class="text-cyan-500">Name : {{ name }} {{ surname }}</div>
+        <div class="text-cyan-500">Age : {{ age }}</div>
+        <figcaption class="font-medium">
+          <div class="text-cyan-500">University : {{ university }}</div>
+          <div class="break-words text-gray-500">
+            Skills :
+            <span v-for="skill in skills" :key="skill">
+              {{ skill }}
+              <span v-if="skill != skills[skills.length - 1]">, </span>
+            </span>
+          </div>
+          <div class="pt-6">
+            <router-link :to="{ name: 'contact' }">
+              <a class="text-gray-500 hover:text-yellow-500">
+                <strong>contact ></strong>
+              </a>
+            </router-link>
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: "Patcharapong",
+    surname: "Chinsuwan",
+    age: 21,
+    university: "Thai-Nichi Institute of Technology",
+    skills: ["ABAP", "Java", "PL/SQL", "SQL"],
+  }),
+};
+</script>
